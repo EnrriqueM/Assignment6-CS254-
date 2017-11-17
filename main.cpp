b951045d098cd5d1af0a36be391c1653d5e89c4d
Enrique <emendoza.2@csu.fullerton.edu>
2017-11-16 18:14:24 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Enrique Mendoza on 11/16/17.
+//  Copyright © 2017 Enrique Mendoza. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, const char* argu[])
+{
+    string filename;            //Holds filename
+    
+    //Check if an argument has been passed
+    if (argc < 2)
+    {
+        cout << "Missing Filename" << endl;
+        return 1;
+    }
+    
+    //Check if file opens
+    ifstream fin(argu[1]);
+    if (!fin)
+    {
+        cout << "File not found" << endl;
+        return 1;
+    }
+    fin.close();
+    
+    //Save source file
+    filename = argu[1];
+    cout << filename << endl;
+    
+    return 0;
+}
