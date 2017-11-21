1f1af8fc17985ea00f8b744fb370a8ff0d5ed7bd
Dat Nguyen <td0615vfx@gmail.com>
2017-11-21 00:51:24 -0800

    Update fileReader.hpp
    Added Type for S to D or D to S

--- fileReader.hpp
+++ fileReader.hpp
@@ -27,6 +27,7 @@ public:
 	vector<string> getData();
 	vector<string> getSize();
 	vector<string> getCycle();
+	vector<string> getType();
 	vector<long int> getlineNumber();
 	long int getTotal() const;
     
@@ -34,10 +35,11 @@ private:
     //Vectors work in parallel
     vector<string> Sample;          //Holds Sample column
     vector<string> RelTime;         //RelTime column
-    vector<long int> Address;       //Address column
+    vector<long int> Address;         //Address column
     vector<string> Data;            //Data column
     vector<string> Size;            //Size column
     vector<string> Cycle;           //Cycle Column
+    vector<string> Type;			//D-to-S or S-to-D
     vector<long int> lineNumber;    //Line number each row appeared
     long int total = 1;             //Total things red
     bool checkAddress(string);      //Check if we need this address stored
