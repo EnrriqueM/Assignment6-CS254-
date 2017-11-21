e8b0ad66bfd0ed5ed024d6456c46481ebf8800fc
walidelmir <33739221+walidelmir@users.noreply.github.com>
2017-11-20 20:01:19 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <fstream>
 
 #include "fileReader.hpp"
+#include "word.hpp"
 
 using namespace std;
 
@@ -42,6 +43,21 @@ int main(int argc, const char* argu[])
     
     //cout << log1.Sample[log1.getTotal() - 1] << endl;
     cout << log1.getTotal() << endl;
+    
+    // Test parsing
+    word w;
+    w.word0("0x8255");
+    w.word1("0xc405");
+    w.word4("0x4000");
+    w.word5("0x0008");
+    w.word10("0x2c6d");
+    w.word15("0x0000");
+    w.word22("0x0005");
+    w.word32("0x1290");
+    w.word37("0x0022");
+    w.word38("0x0f33");
+    w.word40("0x0600");
+    w.word41("0x0801");
     
     return 0;
 }
