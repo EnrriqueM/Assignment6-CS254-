cc6fef296d8f3ec36cf0492557984b494f7cdd18
walidelmir <33739221+walidelmir@users.noreply.github.com>
2017-11-21 05:10:36 -0800

    Add files via upload

---
+++ word.hpp
@@ -0,0 +1,39 @@
+//  CSUF CPSC 254, Fall 2017 Assignment 6
+//
+// Team Members:
+// Walid El Mir
+// David Williams-Haven
+// Dat Nguyen
+// Enrique Mendoza
+//
+// Filename: word.hpp
+
+
+#ifndef word_hpp
+#define word_hpp
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+
+class word{
+public:
+    static void word0(string);
+    static void word1(string);
+    static void word4(string);
+    static void word5(string);
+    static void word10(string);
+    static void word15(string);
+    static void word22(string);
+    static void word32(string);
+    static void word37(string);
+    static void word38(string);
+    static void word40(string);
+    static void word41(string);
+    static long int toInt(string);
+    
+};
+
+#endif /* word_hpp */
