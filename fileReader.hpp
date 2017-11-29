fa852dab5ef7905f97abd55d3cf63db594713b94
walidelmir <33739221+walidelmir@users.noreply.github.com>
2017-11-29 15:09:01 -0800

    Add files via upload

--- fileReader.hpp
+++ fileReader.hpp
@@ -19,30 +19,15 @@
 using namespace std;
 
 class fileRead{
+      
 public:
     fileRead(string);                   // Constructor
-    vector<string> getSample();
-	vector<string> getRelTime();
-	vector<long int> getAddress();
-	vector<string> getData();
-	vector<string> getSize();
-	vector<string> getCycle();
-	vector<string> getType();
-	vector<long int> getlineNumber();
+    
 	long int getTotal() const;
     
-private:
-    //Vectors work in parallel
-    vector<string> Sample;          //Holds Sample column
-    vector<string> RelTime;         //RelTime column
-    vector<long int> Address;         //Address column
-    vector<string> Data;            //Data column
-    vector<string> Size;            //Size column
-    vector<string> Cycle;           //Cycle Column
-    vector<string> Type;			//D-to-S or S-to-D
-    vector<long int> lineNumber;    //Line number each row appeared
-    long int total = 1;             //Total things red
-    bool checkAddress(string);      //Check if we need this address stored
+    void parser(long int, string, string ,string , string);
+    bool checkAddress(string);      //Check if we need this address
+   long int total = 1;
 };
 
 #endif /* fileReader_hpp */
